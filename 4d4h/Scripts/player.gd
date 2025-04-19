23c613336d48463fd77acf759d13c240d7ffa8b9
R2d2cruz <r2d2cruz@gmail.com>
2025-04-19 00:06:24 -0500

    add gravity mechanic

--- 4d4h/Scripts/player.gd
+++ 4d4h/Scripts/player.gd
@@ -4,46 +4,84 @@ const moveSpeed = 50
 const maxSpeed = 100
 const jumpHeight = -350
 const gravity = 15
-const speedGravityLost = 50
+const speedGravityLost = -15
 const timeEyes = 4;
 
 @onready var sprite = $Sprite2D
 @onready var animationPlayer = $AnimationPlayer
 var timerEyes : float = 0;
 var playing_idle2 := false
+var gravity_enabled := true
+
+func _ready():
+	add_to_group("reacts_gravity")
+
+func on_ungravited(state: bool):
+	print("player gravity")
+	gravity_enabled = state
 
 func _physics_process(_delta):
-	velocity.y += gravity
 	var friction = false
+	if gravity_enabled:
+		velocity.y += gravity
 
-	if Input.is_action_pressed("ui_right"):
-		sprite.flip_h = false
-		animationPlayer.play("Walk")
-		velocity.x = min(velocity.x + moveSpeed, maxSpeed)
+		if Input.is_action_pressed("ui_right"):
+			sprite.flip_h = false
+			animationPlayer.play("Walk")
+			velocity.x = min(velocity.x + moveSpeed, maxSpeed)
 
-	elif Input.is_action_pressed("ui_left"):
-		sprite.flip_h = true
-		animationPlayer.play("Walk")
-		velocity.x = max(velocity.x - moveSpeed, -maxSpeed)
+		elif Input.is_action_pressed("ui_left"):
+			sprite.flip_h = true
+			animationPlayer.play("Walk")
+			velocity.x = max(velocity.x - moveSpeed, -maxSpeed)
 
-	else:
-		timerEyes += _delta
-		if timerEyes > timeEyes:
-			animationPlayer.play("Idle_2")
-			playing_idle2 = true
-			if timerEyes > timeEyes + 0.6:
-				timerEyes = 0
 		else:
-			animationPlayer.play("Idle")
-		friction = true
+			timerEyes += _delta
+			if timerEyes > timeEyes:
+				animationPlayer.play("Idle_2")
+				playing_idle2 = true
+				if timerEyes > timeEyes + 0.6:
+					timerEyes = 0
+			else:
+				animationPlayer.play("Idle")
+			friction = true
 
-	if is_on_floor():
-		if Input.is_action_pressed("ui_accept"):
-			velocity.y = jumpHeight
-		if friction:
-			velocity.x = lerp(velocity.x, 0.0, 0.5)
+		if is_on_floor():
+			if Input.is_action_pressed("ui_accept"):
+				velocity.y = jumpHeight
+			if friction:
+				velocity.x = lerp(velocity.x, 0.0, 0.5)
+		else:
+			if friction:
+				velocity.x = lerp(velocity.x, 0.0, 0.01)
+				
 	else:
-		if friction:
-			velocity.x = lerp(velocity.x, 0.0, 0.01)
+		velocity.y = speedGravityLost
+		if is_on_ceiling():
+			if Input.is_action_pressed("ui_right"):
+				sprite.flip_h = false
+				animationPlayer.play("Walk")
+				velocity.x = min(velocity.x + moveSpeed, maxSpeed)
+
+			elif Input.is_action_pressed("ui_left"):
+				sprite.flip_h = true
+				animationPlayer.play("Walk")
+				velocity.x = max(velocity.x - moveSpeed, -maxSpeed)
+				
+			else:
+				timerEyes += _delta
+				if timerEyes > timeEyes:
+					animationPlayer.play("Idle_2")
+					playing_idle2 = true
+					if timerEyes > timeEyes + 0.6:
+						timerEyes = 0
+				else:
+					animationPlayer.play("Idle")
+				friction = true
+				if friction:
+					velocity.x = lerp(velocity.x, 0.0, 0.5)
+		else:
+			if friction:
+				velocity.x = lerp(velocity.x, 0.0, 0.01)
 
 	move_and_slide()
--- 4d4h/Scripts/world_controller.gd
+++ 4d4h/Scripts/world_controller.gd
@@ -1,6 +1,8 @@
 extends Node
 
 var inverted := false
+var gravity := true
+var gravityTimer :float = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,3 +14,13 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("toggle_world"):
 		inverted = !inverted
 		get_tree().call_group("reacts_to_inversion", "on_invert", inverted)
+	if gravity:
+		if Input.is_action_just_pressed("gravity_world"):
+			gravity = false
+			get_tree().call_group("reacts_gravity", "on_ungravited", gravity)
+	else:
+		gravityTimer += delta
+		if gravityTimer > 6:
+			gravity = true
+			gravityTimer = 0
+			get_tree().call_group("reacts_gravity", "on_ungravited", gravity)
