6fa00f4a212d7c2ae3f087fbf400e516d6fe2980
R2d2cruz <r2d2cruz@gmail.com>
2025-04-18 23:06:25 -0500

    create invertible platform

---
+++ 4d4h/Scripts/platform_invertible.gd
@@ -0,0 +1,23 @@
+extends StaticBody2D
+
+@onready var sprite2D = $Sprite2D
+@onready var collisionShape2D := $CollisionShape2D
+@export var appears_when_inverted := false
+@export var frame := 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	add_to_group("reacts_to_inversion")
+	sprite2D.frame = frame
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func on_invert(state: bool):
+	visible = state == appears_when_inverted
+	collisionShape2D.disabled = not visible
+	sprite2D.visible = visible
+
+func get_key_by_value(dict: Dictionary, value: int) -> int:
+	for k in dict.keys():
+		if dict[k] == value:
+			return k
+	return -1
