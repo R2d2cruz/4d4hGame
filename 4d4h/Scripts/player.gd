80072a65538cb71c32608a722923ce54f6a8d2ab
R2d2cruz <r2d2cruz@gmail.com>
2025-04-19 15:06:26 -0500

    add collition with pushables

--- 4d4h/Scripts/player.gd
+++ 4d4h/Scripts/player.gd
@@ -1,20 +1,25 @@
 extends CharacterBody2D
 
-const moveSpeed = 50
-const maxSpeed = 100
-const jumpHeight = -350
-const gravity = 15
-const speedGravityLost = -15
-const timeEyes = 4;
+const moveSpeed := 50
+const maxSpeed := 100
+const jumpHeight := -350
+const gravity := 15
+const speedGravityLost := -15
+const timeEyes := 4;
 
-@onready var sprite = $Sprite2D
-@onready var animationPlayer = $AnimationPlayer
+@onready var sprite := $Sprite2D
+@onready var animationPlayer := $AnimationPlayer
+@onready var detectarCaja : RayCast2D = $DetectarCaja
+
 var timerEyes : float = 0;
 var playing_idle2 := false
 var gravity_enabled := true
+var last_pushable : Node = null
+
 
 func _ready():
-	add_to_group("reacts_gravity")
+	pass
+	#add_to_group("reacts_gravity")
 
 func on_ungravited(state: bool):
 	print("player gravity")
@@ -23,15 +28,17 @@ func on_ungravited(state: bool):
 func _physics_process(_delta):
 	var friction = false
 	if gravity_enabled:
-		velocity.y += gravity
+		velocity += get_gravity() * _delta
 
-		if Input.is_action_pressed("ui_right"):
+		if Input.is_action_pressed("right"):
 			sprite.flip_h = false
+			detectarCaja.scale.x = 1
 			animationPlayer.play("Walk")
 			velocity.x = min(velocity.x + moveSpeed, maxSpeed)
 
-		elif Input.is_action_pressed("ui_left"):
+		elif Input.is_action_pressed("left"):
 			sprite.flip_h = true
+			detectarCaja.scale.x = -1
 			animationPlayer.play("Walk")
 			velocity.x = max(velocity.x - moveSpeed, -maxSpeed)
 
@@ -45,6 +52,15 @@ func _physics_process(_delta):
 			else:
 				animationPlayer.play("Idle")
 			friction = true
+		if detectarCaja.is_colliding():
+			var collider = detectarCaja.get_collider()
+			if collider.is_in_group("pushable"):
+				collider.direction = Input.get_axis("left", "right")
+				last_pushable = collider
+		else:
+			if last_pushable != null:
+				last_pushable.direction = 0
+				last_pushable = null
 
 		if is_on_floor():
 			if Input.is_action_pressed("ui_accept"):
@@ -57,31 +73,16 @@ func _physics_process(_delta):
 				
 	else:
 		velocity.y = speedGravityLost
-		if is_on_ceiling():
-			if Input.is_action_pressed("ui_right"):
-				sprite.flip_h = false
-				animationPlayer.play("Walk")
-				velocity.x = min(velocity.x + moveSpeed, maxSpeed)
-
-			elif Input.is_action_pressed("ui_left"):
-				sprite.flip_h = true
-				animationPlayer.play("Walk")
-				velocity.x = max(velocity.x - moveSpeed, -maxSpeed)
-				
-			else:
-				timerEyes += _delta
-				if timerEyes > timeEyes:
-					animationPlayer.play("Idle_2")
-					playing_idle2 = true
-					if timerEyes > timeEyes + 0.6:
-						timerEyes = 0
-				else:
-					animationPlayer.play("Idle")
-				friction = true
-				if friction:
-					velocity.x = lerp(velocity.x, 0.0, 0.5)
+		timerEyes += _delta
+		if timerEyes > timeEyes:
+			animationPlayer.play("Idle_2")
+			playing_idle2 = true
+			if timerEyes > timeEyes + 0.6:
+				timerEyes = 0
 		else:
-			if friction:
-				velocity.x = lerp(velocity.x, 0.0, 0.01)
+			animationPlayer.play("Idle")
+		friction = true
+		if friction:
+			velocity.x = lerp(velocity.x, 0.0, 0.5)
 
 	move_and_slide()
