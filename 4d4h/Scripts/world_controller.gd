eab2f5c41201a5cd0b93d2ba7719b15546ca3943
R2d2cruz <r2d2cruz@gmail.com>
2025-04-20 12:58:15 -0500

    add background tilemap

--- 4d4h/Scripts/world_controller.gd
+++ 4d4h/Scripts/world_controller.gd
@@ -4,15 +4,19 @@ var inverted := false
 var gravity := true
 var gravityTimer : float = 0.0
 @export var gravityTimeLimit : float = 5.0
+@export var can_invert := true
+@export var startInvert := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if startInvert:
+		inverted = !inverted
+		get_tree().call_group("reacts_to_inversion", "on_invert", inverted)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("toggle_world"):
+	if Input.is_action_just_pressed("toggle_world") and can_invert:
 		inverted = !inverted
 		get_tree().call_group("reacts_to_inversion", "on_invert", inverted)
 	if gravity:
