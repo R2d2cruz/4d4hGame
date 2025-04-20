aac23657d5fe7eaf89677f13d3fd164fb1b7ed63
R2d2cruz <r2d2cruz@gmail.com>
2025-04-20 14:16:21 -0500

    Update tile_mapController.gd

--- 4d4h/Scripts/tile_mapController.gd
+++ 4d4h/Scripts/tile_mapController.gd
@@ -20,6 +20,7 @@ extends TileMapLayer
 	Vector3i(0, 3, 2): Vector3i(3, 3, 2),
 	Vector3i(0, 4, 2): Vector3i(3, 4, 2),
 	Vector3i(0, 5, 2): Vector3i(3, 5, 2),
+	Vector3i(0, 1, 3): Vector3i(3, 1, 3),
 	Vector3i(0, 2, 3): Vector3i(3, 2, 3),
 	Vector3i(0, 3, 3): Vector3i(3, 3, 3),
 	Vector3i(0, 4, 3): Vector3i(3, 4, 3),
